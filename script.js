be4eb9d379da27b725a548daae4920b1fb19a8a2
Zach <zachjensz@protonmail.com>
2021-02-04 13:16:42 +1100

    Basic page and functionality

---
+++ script.js
@@ -0,0 +1,23 @@
+const addVisitButton = document.querySelector('#add-visit')
+const visits = document.querySelector('#visits')
+const visitTemplate = document.querySelector('#visit-template')
+let visitNumber = 2
+
+addVisitButton.addEventListener('click', (e) => {
+    e.preventDefault()
+    const visit = visitTemplate.content.cloneNode(true)
+    visit
+        .querySelector('.visit-date-label')
+        .setAttribute('for', `visit-date-${visitNumber}`)
+    visit
+        .querySelector('.visit-date')
+        .setAttribute('name', `visit-date-${visitNumber}`)
+    visit
+        .querySelector('.visit-hours-label')
+        .setAttribute('for', `visit-hours-${visitNumber}`)
+    visit
+        .querySelector('.visit-hours')
+        .setAttribute('name', `visit-hours-${visitNumber}`)
+    visits.append(visit)
+    visitNumber++
+})
