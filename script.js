07bdd78c6b2de56a78f8fe8452186cf8c2070f78
Zach <zachjensz@protonmail.com>
2021-02-09 12:40:16 +1100

    Added total hours calculator, minor styling fixes

--- script.js
+++ script.js
@@ -9,23 +9,50 @@ addVisitButton.addEventListener('click', (e) => {
     const visit = visitTemplate.content.cloneNode(true)
     visit
         .querySelector('.visit-date-label')
-        .setAttribute('for', `visit-date ${visitNumber}`)
+        .setAttribute('for', `visit-date-${visitNumber}`)
     visit
         .querySelector('.visit-date')
-        .setAttribute('name', `visit-date ${visitNumber}`)
+        .setAttribute('name', `visit-date-${visitNumber}`)
     visit
         .querySelector('.visit-type-label')
-        .setAttribute('name', `visit-type ${visitNumber}`)
+        .setAttribute('name', `visit-type-${visitNumber}`)
     visit
         .querySelector('.visit-type')
-        .setAttribute('name', `visit-type ${visitNumber}`)
+        .setAttribute('name', `visit-type-${visitNumber}`)
     visit
         .querySelector('.visit-hours-label')
-        .setAttribute('for', `visit-hours ${visitNumber}`)
+        .setAttribute('for', `visit-hours-${visitNumber}`)
     visit
         .querySelector('.visit-hours')
-        .setAttribute('name', `visit-hours ${visitNumber}`)
-
+        .setAttribute('name', `visit-hours-${visitNumber}`)
     visits.append(visit)
+
+    document
+        .querySelector(`[name=visit-hours-${visitNumber}]`)
+        .addEventListener('input', addTotalHours)
+
     visitNumber++
 })
+
+// Add total hours
+function addTotalHours() {
+    let visitHoursFields = document.querySelectorAll('[name*=visit-hours]')
+    console.log('visitHoursFields: ', visitHoursFields)
+    let totalHours = 0
+    visitHoursFields.forEach((visit) => {
+        totalHours += Number(visit.value)
+    })
+    console.log('totalHours', totalHours)
+    /*
+    console.log(visitHours)
+    let totalHours = visitHours.reduce((acc, cur) => {
+        return (acc += cur.value)
+    }, 0)
+    */
+    document.querySelector('#total-hours').innerText = totalHours
+}
+
+// Add event listener to first visit
+document
+    .querySelector('[name=visit-hours-1]')
+    .addEventListener('input', addTotalHours)
